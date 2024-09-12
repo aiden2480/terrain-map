cee4b4e290f74fa375b34833bb1a92284f84dc04
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-09-12 17:41:46 +1000

    Add initial project files

---
+++ TerrainMap/BackgroundWorker.cs
@@ -0,0 +1,22 @@
+﻿using Blazor.BrowserExtension;
+using System.Threading.Tasks;
+
+namespace TerrainMap;
+
+public partial class BackgroundWorker : BackgroundWorkerBase
+{
+    [BackgroundWorkerMain]
+    public override void Main()
+    {
+        WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+    }
+
+    async Task OnInstalled()
+    {
+        var indexPageUrl = await WebExtensions.Runtime.GetURL("index.html");
+        await WebExtensions.Tabs.Create(new()
+        {
+            Url = indexPageUrl
+        });
+    }
+}
---
+++ TerrainMap/Program.cs
@@ -0,0 +1,28 @@
+﻿using Blazor.BrowserExtension;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using TerrainMap;
+
+// Create builder & configure browser extension
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.UseBrowserExtension(browserExtension =>
+{
+    if (browserExtension.Mode == BrowserExtensionMode.Background)
+    {
+        builder.RootComponents.AddBackgroundWorker<BackgroundWorker>();
+    }
+    else
+    {
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+    }
+});
+
+// Add services & run
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
