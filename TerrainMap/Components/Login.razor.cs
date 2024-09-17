8053ed53f94a750af74f35e6869962080381ea06
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-09-17 21:35:40 +1000

    Create login component

---
+++ TerrainMap/Components/Login.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Threading.Tasks;
+using TerrainMap.Models;
+using TerrainMap.Services.Interface;
+
+namespace TerrainMap.Components;
+
+public partial class Login : ComponentBase
+{
+    [Inject]
+    public required ITerrainAuthService TerrainAuthService { get; set; }
+
+    [Inject]
+    public required ILocalAuthService AuthService { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public EventCallback OnLoginSuccess { get; set; }
+
+    private MudForm form = default!;
+
+    private Branch selectedBranch = Branch.NSW;
+
+    private uint? memberNumber;
+
+    private string password = string.Empty;
+
+    private string errorMessage = string.Empty;
+
+    private bool isLoading = false;
+
+    async Task SignIn()
+    {
+        // Ensure form is valid before proceeding
+        await form.Validate();
+
+        if (!form.IsValid)
+        {
+            return;
+        }
+
+        // Disable button & display feedback while we validate
+        isLoading = true;
+        var loginApiResponse = await TerrainAuthService.AttemptLoginWithCredentials(selectedBranch, (uint)memberNumber!, password);
+
+        // If successful, invoke callback. Otherwise display error
+        if (loginApiResponse.Success)
+        {
+            await AuthService.UpdateFromLoginApiResponse(loginApiResponse);
+            await OnLoginSuccess.InvokeAsync();
+        }
+        else
+        {
+            errorMessage = loginApiResponse.ErrorMessage!;
+            isLoading = false;
+        }
+    }
+}
