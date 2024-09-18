dc5b1ccc1b4bdf5c32de9115bd1c9600171d519c
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-09-18 23:28:37 +1000

    Approval comment dialog

---
+++ TerrainMap/Components/ApprovalCommentDialog.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace TerrainMap.Components;
+
+public partial class ApprovalCommentDialog : ComponentBase
+{
+    [CascadingParameter]
+    public required MudDialogInstance Dialog { get; init; }
+
+    [Parameter]
+    [EditorRequired]
+    public required bool IsApproval { get; init; }
+
+    private string comment = string.Empty;
+    
+    private string SubmitText
+        => IsApproval ? "Approve" : "Improve";
+
+    private Color SubmitColor
+        => IsApproval ? Color.Primary : Color.Secondary;
+
+    private void Submit()
+        => Dialog.Close(comment);
+
+    private void Cancel()
+        => Dialog.Cancel();
+}
