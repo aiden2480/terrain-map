e645fbd88539bb17a482e9b70b784fb5ca2a6cc4
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-10-12 19:28:40 +1100

    Move pending approval to its own class

---
+++ TerrainMap/Components/PendingApprovalPanel.razor.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TerrainMap.Models;
+using TerrainMap.Services.Interface;
+
+namespace TerrainMap.Components;
+
+public partial class PendingApprovalPanel : ComponentBase
+{
+    [Inject]
+    public required ITerrainApprovalService TerrainApprovalService { get; set; }
+
+    [Inject]
+    public required ITerrainAchievementService TerrainAchievementService { get; set; }
+
+    [Inject]
+    public required ITerrainTemplateService TerrainTemplateService { get; set; }
+
+    [Inject]
+    public required IDialogService DialogService { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public required Approval Approval { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public required Profile CurrentProfile { get; init; }
+
+    Achievement Achievement = null!;
+
+    IEnumerable<ApprovalInput> Inputs = [];
+
+    string comment = string.Empty;
+
+    bool DataIsLoaded
+        => Achievement is not null && Inputs is not null;
+
+    string PanelText
+        => $"{Approval.Member.FirstName}'s {TerrainApprovalService.GetApprovalDescriptionAndSvg(Approval).Description}";
+
+    int ApproveCount
+        => Approval.Submission.ActionedBy.Count(a => a.Outcome == "approved");
+
+    string PluralApproveText
+        => ApproveCount == 1 ? "approve" : "approves";
+
+    int ImproveCount
+        => Approval.Submission.ActionedBy.Count(a => a.Outcome == "rejected");
+
+    string PluralImproveText
+        => ImproveCount == 1 ? "improve" : "improves";
+
+    bool CanActionApproval
+        => !SubmittedByCurrentUser && !AlreadyActionedByCurrentUser;
+
+    bool SubmittedByCurrentUser
+        => Approval.Member.Id == CurrentProfile.Member.Id;
+
+    bool AlreadyActionedByCurrentUser
+        => Approval.Submission.ActionedBy
+            .Select(a => a.Id)
+            .Contains(CurrentProfile.Member.Id);
+
+    string GetPanelIconStyle()
+    {
+        var icon = TerrainApprovalService.GetApprovalDescriptionAndSvg(Approval).Icon;
+        var iconUrl = Path.Combine("icons", icon.ToString().ToLower() + ".svg");
+
+        return $"content: url({iconUrl}); font-size: 1.9rem";
+    }
+
+    async Task ExpandedChanged(bool isOpen)
+    {
+        if (isOpen && !DataIsLoaded)
+        {
+            Achievement = await TerrainAchievementService.GetAchievement(Approval);
+            Inputs = await TerrainTemplateService.GetInputs(Achievement);
+        }
+    }
+
+    async Task ApproveAchievement()
+    {
+        var continueAction = await ShowCommentDialog(true);
+
+        if (continueAction)
+        {
+            await TerrainAchievementService.ApproveSubmission(Approval.Submission, comment);
+            Approval = await TerrainApprovalService.RefreshApproval(CurrentProfile.Unit!.Id, Approval);
+        }
+    }
+
+    async Task ImproveAchievement()
+    {
+        var continueAction = await ShowCommentDialog(false);
+
+        if (continueAction)
+        {
+            await TerrainAchievementService.ImproveSubmission(Approval.Submission, comment);
+            Approval = await TerrainApprovalService.RefreshApproval(CurrentProfile.Unit!.Id, Approval);
+        }
+    }
+
+    async Task<bool> ShowCommentDialog(bool isApproval)
+    {
+        var parameters = new DialogParameters<ApprovalCommentDialog>
+        {
+            { d => d.IsApproval, isApproval },
+            { d => d.Comment, comment }
+        };
+
+        var title = isApproval ? $"Approve {PanelText}?" : $"Improve {PanelText}?";
+        var dialog = await DialogService.ShowAsync<ApprovalCommentDialog>(title, parameters);
+        var result = await dialog.Result;
+
+        return !result!.Canceled;
+    }
+}
