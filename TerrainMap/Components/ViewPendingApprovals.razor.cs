4648f22bad7d01583e35369ee1ee15c46a7c4743
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-10-12 19:58:42 +1100

    Display MudSkeleton while approvals are loading

---
+++ TerrainMap/Components/ViewPendingApprovals.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TerrainMap.Models;
+using TerrainMap.Services.Interface;
+
+namespace TerrainMap.Components;
+
+public partial class ViewPendingApprovals : ComponentBase
+{
+    [Inject]
+    public required ITerrainApprovalService TerrainApprovalService { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public required Profile CurrentProfile { get; init; }
+
+    private IEnumerable<Approval>? pendingApprovals;
+
+    protected override async Task OnInitializedAsync()
+    {
+        pendingApprovals = await TerrainApprovalService.GetPendingApprovals(CurrentProfile.Unit!.Id);
+    }
+}
