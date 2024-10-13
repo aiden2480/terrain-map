f59dde0b7ceaec07a61a0d23e1aa21e1cd4ccf40
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-10-13 16:24:29 +1100

    Check member unit council status in popup

---
+++ TerrainMap/Pages/Popup.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TerrainMap.Models;
+using TerrainMap.Services.Interface;
+
+namespace TerrainMap.Pages;
+
+public partial class Popup : ComponentBase
+{
+    [Inject]
+    public required IStorageService StorageService { get; set; }
+
+    [Inject]
+    public required ITerrainApprovalService TerrainApprovalService { get; set; }
+
+    [Inject]
+    public required ITerrainProfileService TerrainProfileService { get; set; }
+
+    async Task<Profile?> GetFirstProfileInUnitCouncil(bool forceRefreshCache = false)
+        => (await GetProfiles(forceRefreshCache))
+            .Where(p => p.Unit is not null)
+            .Where(p => p.Unit!.Roles.Contains("unit-council"))
+            .FirstOrDefault();
+
+    async Task<IEnumerable<Profile>> GetProfiles(bool forceRefreshCache)
+    {
+        var profiles = await StorageService.GetProfilesFromStorage();
+
+        if (profiles is null || forceRefreshCache)
+        {
+            profiles = await TerrainProfileService.GetProfiles();
+            await StorageService.SetProfiles(profiles);
+        }
+
+        return profiles;
+    }
+}
